b7dfdcc5f230737db5682d6403f15eaefaa767be
Karol Świerczek <karol.sw3@gmail.com>
2018-02-28 21:50:31 +0100

    Change made projects list

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ var app = new Vue({
   el: '#projects',
   data: {
     projects: [
-      {title: 'Telackes.com', description: 'A most recent project which I have made using Vue.js as the main framework and Joomla as CMS'},
+      {title: 'Gradientify.js', description: 'A simple javascript library for creating animated gradient backgrounds'},
       {title: 'Lakiery.hybrydowe.eu', description: 'A website which showcases products in a user-friendly style'},
-      {title: 'My portfolio', description: 'This portfolio website has been written using Vue.js and Vanilla javascript.'},
+      {title: 'My portfolio', description: 'This portfolio website has been written using Vue.js and Vanilla javascript'},
       {title: '55+ Pens on codepen.io', description: 'Every now and then I am making a simple sketches of websites and applications. I have made about 55 various projects of this type'}
     ]
   }
