25a4a64182bc1d8a824799ead418e55c16c1a90f
karolsw2 <karol.sw3@gmail.com>
2018-06-01 12:24:20 +0200

    Divide projects into primary and normal (with and without images)

--- js/index.js
+++ js/index.js
@@ -7,8 +7,8 @@ var app = new Vue({
       {title: 'Gradientify.js', about: 'Javascript library for creating animated gradient backgrounds', annotation: 'Pure JS', image: './images/projects/gradientify.png', url: 'https://karolsw2.github.io/gradientify.js/'},
       {title: 'Cookieclicker', about: 'Cookieclicker game written in ES6', annotation: 'Node.js', image: './images/projects/cookieclicker.png', url: 'https://github.com/karolsw2/Cookieclicker'},
       {title: 'Lakiery.hybrydowe.eu', about: 'A website for cosmetics seller', annotation: 'HTML & CSS', image: './images/projects/lakiery.png', url: 'http://lakiery.hybrydowe.eu/'},
-      {title: 'This portfolio', about: 'The page you are currently browsing', annotation: 'Vue.js', image: './images/projects/portfolio.png', url: ''},
-      {title: '65+ Pens on codepen.io', about: 'My front-end portfolio of website sketches', annotation: 'Various', image: './images/projects/codepen.png', url: 'https://codepen.io/karolsw2/'}
+      {title: 'This portfolio', about: 'The page you are currently browsing', annotation: 'Vue.js', url: ''},
+      {title: '65+ Pens on codepen.io', about: 'My front-end portfolio of website sketches', annotation: 'Various', url: 'https://codepen.io/karolsw2/'}
     ]
   }
 })
