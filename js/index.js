043ba723cd403eff105a790ee73eed8e8e2f6e6c
karolsw2 <karol.sw3@gmail.com>
2018-06-01 13:11:33 +0200

    Remove images from primary projects

--- js/index.js
+++ js/index.js
@@ -4,9 +4,11 @@ var app = new Vue({
   el: '#projects',
   data: {
     projects: [
-      {title: 'Gradientify.js', about: 'Javascript library for creating animated gradient backgrounds', annotation: 'Pure JS', image: './images/projects/gradientify.png', url: 'https://karolsw2.github.io/gradientify.js/'},
-      {title: 'Cookieclicker', about: 'Cookieclicker game written in ES6', annotation: 'Node.js', image: './images/projects/cookieclicker.png', url: 'https://github.com/karolsw2/Cookieclicker'},
-      {title: 'Lakiery.hybrydowe.eu', about: 'A website for cosmetics seller', annotation: 'HTML & CSS', image: './images/projects/lakiery.png', url: 'http://lakiery.hybrydowe.eu/'},
+      {title: 'Gradientify.js', about: 'Javascript library for creating animated gradient backgrounds', annotation: 'Pure JS', url: 'https://karolsw2.github.io/gradientify.js/'},
+      {title: 'Lakiery.hybrydowe.eu', about: 'A website for cosmetics seller', annotation: 'HTML & CSS', url: 'http://lakiery.hybrydowe.eu/'},
+      {title: 'Cookieclicker', about: 'Cookieclicker game written in ES6', annotation: 'Node.js', url: 'https://github.com/karolsw2/Cookieclicker'},
+      {title: 'NOVA-Transport', about: 'A webiste template for transport companies', annotation: 'HTML & CSS', url: 'https://karolsw2.github.io/NOVA-Transport/'},
+      {title: 'Minesweeper', about: 'Object-oriented minesweeper adaptation', annotation: 'ES6', url: 'https://karolsw2.github.io/Minesweeper/'},
       {title: 'This portfolio', about: 'The page you are currently browsing', annotation: 'Vue.js', url: ''},
       {title: '65+ Pens on codepen.io', about: 'My front-end portfolio of website sketches', annotation: 'Various', url: 'https://codepen.io/karolsw2/'}
     ]
