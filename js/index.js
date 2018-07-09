d26c76d516fc854ef1ed0b5895221094f0976ef8
karolsw3 <karol.sw3@gmail.com>
2018-07-09 07:55:35 +0200

    Update projects

--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ var app = new Vue({
   el: '#projects',
   data: {
     projects: [
-      {title: 'Gradientify.js', about: 'Javascript library for creating animated gradient backgrounds', annotation: 'Pure JS', url: 'https://karolsw2.github.io/gradientify.js/'},
+      {title: 'Gradientify.js', about: 'Javascript library for creating animated gradient backgrounds', annotation: 'Pure JS', url: 'https://karolsw3.github.io/gradientify.js/'},
       {title: 'Lakiery.hybrydowe.eu', about: 'A website for cosmetics seller', annotation: 'HTML & CSS', url: 'http://lakiery.hybrydowe.eu/'},
-      {title: 'Cookieclicker', about: 'Cookieclicker game written in ES6', annotation: 'Node.js', url: 'https://github.com/karolsw2/Cookieclicker'},
-      {title: 'NOVA-Transport', about: 'A webiste template for transport companies', annotation: 'HTML & CSS', url: 'https://karolsw2.github.io/NOVA-Transport/'},
-      {title: 'Minesweeper', about: 'Object-oriented minesweeper adaptation', annotation: 'ES6', url: 'https://karolsw2.github.io/Minesweeper/'},
+      {title: 'Sudoku', about: 'A modern, API-based sudoku web app', annotation: 'Node.js', url: 'https://github.com/Galactim/Sudoku'},
+      {title: 'NOVA-Transport', about: 'A webiste template for transport companies', annotation: 'HTML & CSS', url: 'https://karolsw3.github.io/NOVA-Transport/'},
+      {title: 'Minesweeper', about: 'Object-oriented minesweeper adaptation', annotation: 'ES6', url: 'https://karolsw3.github.io/Minesweeper/'},
       {title: 'This portfolio', about: 'The page you are currently browsing', annotation: 'Vue.js', url: ''},
       {title: '65+ Pens on codepen.io', about: 'My front-end portfolio of website sketches', annotation: 'Various', url: 'https://codepen.io/karolsw2/'}
     ]
